9340f02d36789937c489b277c36b23cca795a04a
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2023-10-19 12:55:03 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,13 +1,32 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
- * Hello world!
- *
+ * Utilización del framework Spark.
+ * Hola, mundo!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hola, mundo!" );
+public class App {
+    public static void main(String[] args) {
+        get("/",
+                (request, response) -> "<h1>¡Index!</h1>"
+
+        );
+
+        get("/ruta2",
+                (request, response) -> "<h1>¡Hola, mundillo!</h1>"
+
+        );
+
+        get("/ruta3",
+                (request, response) -> "<h1>¡Adiós, mundillo!</h1>"
+
+        );
+
+        get("/json",
+                (request, response) -> "('nombre':'Alumno','Matrícula':'S2000','Carrera':'Estadística')"
+
+        );
+
     }
 }
