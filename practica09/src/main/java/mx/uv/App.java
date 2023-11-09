b6b0565b1f0a1b4db2a39050e10c9634560e6158
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2023-11-09 13:44:40 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -7,26 +7,41 @@ import static spark.Spark.*;
  * Hola, mundo!
  */
 public class App {
-    public static void main(String[] args) {
-        get("/",
-                (request, response) -> "<h1>¡Index!</h1>"
+        public static void main(String[] args) {
+                // fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+                options("/*", (request, response) -> {
+                        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+                        if (accessControlRequestHeaders != null) {
+                                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                        }
 
-        );
+                        String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                        if (accessControlRequestMethod != null) {
+                                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                        }
+                        return "OK";
+                });
+                before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
-        get("/ruta2",
-                (request, response) -> "<h1>¡Hola, mundillo!</h1>"
+                get("/",
+                                (request, response) -> "<h1>¡Index!</h1>"
 
-        );
+                );
 
-        get("/ruta3",
-                (request, response) -> "<h1>¡Adiós, mundillo!</h1>"
+                get("/ruta2",
+                                (request, response) -> "<h1>¡Hola, mundillo!</h1>"
 
-        );
+                );
 
-        get("/json",
-                (request, response) -> "('nombre':'Alumno','Matrícula':'S2000','Carrera':'Estadística')"
+                get("/ruta3",
+                                (request, response) -> "<h1>¡Adiós, mundillo!</h1>"
 
-        );
+                );
 
-    }
+                get("/json",
+                                (request, response) -> "{\"alumno\":\"John\",\"matricula\":\"S2000\",\"carrera\":\"TC\"}"
+
+                );
+
+        }
 }
